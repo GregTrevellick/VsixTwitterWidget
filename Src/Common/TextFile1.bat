7eb42b56864f67e853c88f17038a1d67c30ca7b6
Greg Trevellick <gtrevellick@googlemail.com>
2018-06-19 22:24:40 +0100

    .

--- Src/Common/TextFile1.bat
+++ Src/Common/TextFile1.bat
@@ -1,2 +1,12 @@
-start  C:\Users\gtrev\Source\Repos\VsixTwitterWidget\Src\@Vsts\app\dashboard-widget.html
+REM launch widgets in browsers
 start  C:\Users\gtrev\Source\Repos\VsixTwitterWidget\Src\@DevHumor\app\dashboard-widget.html
+start  C:\Users\gtrev\Source\Repos\VsixTwitterWidget\Src\@Vsts\app\dashboard-widget.html
+
+
+
+REM re-gen vsix's
+cd C:\Users\gtrev\Source\Repos\VsixTwitterWidget\Src\@DevHumor\
+tfx extension create --manifest-globs vss-extension.json
+cd C:\Users\gtrev\Source\Repos\VsixTwitterWidget\Src\@Vsts\
+tfx extension create --manifest-globs vss-extension.json
+
