39b724bea4300cd92db1242f612934fa8e4350cf
Greg Trevellick <gtrevellick@googlemail.com>
2018-06-19 21:36:00 +0100

    .

---
+++ Src/Common/TextFile1.bat
@@ -0,0 +1,2 @@
+start  C:\Users\gtrev\Source\Repos\VsixTwitterWidget\Src\@Vsts\app\dashboard-widget.html
+start  C:\Users\gtrev\Source\Repos\VsixTwitterWidget\Src\@DevHumor\app\dashboard-widget.html
