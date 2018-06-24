1d5cfc3bb8490bfc40882b9c8760bbeb6a92ee6a
Greg Trevellick <gtrevellick@googlemail.com>
2018-06-24 14:16:18 +0100

    .

--- Src/Common/Builder.bat
+++ Src/Common/Builder.bat
@@ -1,19 +1,20 @@
+rem  /S    Copy folders and subfolders
+rem  /Y    Suppress prompt to confirm overwriting a file.
+rem  /F    Display full source and destination file names while copying.
+rem  /R    Overwrite read-only files.
+
 echo "SolutionDir" %1
 echo "ProjectDir" %2
 
 FOR %%G IN (devhumor,vsts) DO (
 
-	REM copy the static files to each of the projects. The manifest json file only allows paths that are children of its own directory, hence can't specify the common files themselves in the manifest json files. Also, shortcut file links won't do the trick either. Do not put the copied files into .gitignore as they are part of final vsix file and hence worthy of source control. /r Overwrites read-only files. /y Overwrites existing files without prompting you.
-	xcopy /R /Y %2artefacts\Licence.md %1@%%G\artefacts\Licence.md
-
-	REM copy javascript files
-	xcopy %2app\Scripts\SDK\* %1@%%G\app\Scripts\SDK\ /E /I /Y /S 
-	xcopy %2app\Scripts\app.js %1@%%G\app\Scripts\app.js /R /Y
-	xcopy %2app\Scripts\styling.js %1@%%G\app\Scripts\styling.js /R /Y 
+	REM copy the static files to each of the projects. The manifest json file only allows paths that are children of its own directory, hence can't specify the common files themselves in the manifest json files. Also, shortcut file links won't do the trick either.
+	xcopy %2app\Scripts\* %1@%%G\app\Scripts\ /F /R /Y /S
+	xcopy %2artefacts\Licence.md %1@%%G\artefacts\Licence.md /F /R /Y
 
 	REM make the new files read-only
-	attrib +R %1@%%G\artefacts\Licence.md
 	attrib +R %1@%%G\app\Scripts\*
+	attrib +R %1@%%G\artefacts\Licence.md
 
 	REM launch widgets in browsers
 	start  %1@%%G\app\dashboard-widget.html
@@ -21,5 +22,4 @@ FOR %%G IN (devhumor,vsts) DO (
 	REM re-gen vsix's 
 	cd %1@%%G\
 	tfx extension create --rev-version --manifest-globs vss-extension.json
-
 )
