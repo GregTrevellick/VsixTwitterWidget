10e85b6a36466bf6ba3133b9fb5a6bb9bbe37e1b
Greg Trevellick <gtrevellick@googlemail.com>
2018-06-27 08:17:45 +0100

    .

--- Src/@Dev_Humor/artefacts/Overview.md
+++ Src/@Dev_Humor/artefacts/Overview.md
@@ -1,3 +1,3 @@
 Brighten up you and your team's day by showing [Dev Humor's tweets](https://twitter.com/dev_humor) in your dashboard.
 
-![](https://github.com/GregTrevellick/VsixTwitterWidget/blob/master/Src/@DevHumor/artefacts/Screenshot.png?raw=true)
+![](https://github.com/GregTrevellick/VsixTwitterWidget/blob/master/Src/@Dev_Humor/artefacts/Screenshot.png?raw=true)
--- Src/Common/Builder.bat
+++ Src/Common/Builder.bat
@@ -6,7 +6,7 @@ rem  /R    Overwrite read-only files.
 echo "SolutionDir" %1
 echo "ProjectDir" %2
 
-FOR %%G IN (azure,ch9,code,codeproject,dailydotnettips,devhumor,dotnet,dotnetcurry,dotnetfdn,dotnetkicks,mscloud,newsycombinator,nodejs,octopusdeploy,reddittech,theregister,visualstudio,vsmdev,vsts,xamarinhq) DO (
+FOR %%G IN (azure,ch9,code,codeproject,dailydotnettips,dev_humor,dotnet,dotnetcurry,dotnetfdn,dotnetkicks,mscloud,newsycombinator,nodejs,octopusdeploy,reddittech,theregister,visualstudio,vsmdev,vsts,xamarinhq) DO (
 
 	REM copy the static files to each of the projects. The manifest json file only allows paths that are children of its own directory, hence can't specify the common files themselves in the manifest json files. Also, shortcut file links won't do the trick either.
 	xcopy %2app\Scripts\* %1@%%G\app\Scripts\ /F /R /Y /S
@@ -17,7 +17,7 @@ FOR %%G IN (azure,ch9,code,codeproject,dailydotnettips,devhumor,dotnet,dotnetcur
 	attrib +R %1@%%G\artefacts\Licence.md
 
 	REM launch widgets in browsers
-	start  %1@%%G\app\dashboard-widget.html
+	rem start  %1@%%G\app\dashboard-widget.html
 
 	REM re-gen vsix's 
 	cd %1@%%G\
