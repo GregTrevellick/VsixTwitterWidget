1f0aa152ef213cd4fc57be754c83ac3c108cbec2
GregTrevellick <gtrevellick@googlemail.com>
2018-09-11 07:30:37 +0100

    added @azuredevops

---
+++ Src/@AzureDevOps/app/Scripts/common.js
@@ -0,0 +1,57 @@
+var vssRegisterName = "VSSRegisterName_TweetAny";
+
+//https://github.com/Microsoft/vss-web-extension-sdk
+
+VSS.init({
+    explicitNotifyLoaded: true,
+    usePlatformStyles: true
+});
+
+VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
+
+    //https://docs.microsoft.com/en-us/vsts/extend/develop/styles-from-widget-sdk?view=vsts
+    WidgetHelpers.IncludeWidgetStyles();
+    WidgetHelpers.IncludeWidgetConfigurationStyles();
+
+    var registerWidget = function () {
+        return {
+            load: function (widgetSettings) {
+                return WidgetHelpers.WidgetStatusHelper.Success();
+            }
+        }
+    }
+
+    VSS.register(vssRegisterName, registerWidget);
+
+    VSS.notifyLoadSucceeded();
+});
+
+
+(function ($) {
+
+    /**
+    * @function
+    * @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+    * @param {function} handler A function to execute at the time when the element is inserted
+    * @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+    * @example $(selector).waitUntilExists(function);
+    */
+
+    $.fn.waitUntilExists = function (handler, shouldRunHandlerOnce, isChild) {
+        var found = 'found';
+        var $this = $(this.selector);
+        var $elements = $this.not(function () { return $(this).data(found); }).each(handler).data(found, true);
+
+        if (!isChild) {
+            (window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+                window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500)
+                ;
+        }
+        else if (shouldRunHandlerOnce && $elements.length) {
+            window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+        }
+
+        return $this;
+    }
+
+}(jQuery));
