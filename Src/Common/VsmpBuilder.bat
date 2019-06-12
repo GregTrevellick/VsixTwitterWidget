9ecaafcd2039607935e60b3018aca90cdccbc15b
Greg Trevellick <gtrevellick@googlemail.com>
2019-06-12 18:51:59 +0100

    .privacy policy

--- Src/Common/VsmpBuilder.bat
+++ Src/Common/VsmpBuilder.bat
@@ -1,3 +1,7 @@
+rem vsmp_pat not used
+rem no need to run this bat file in cmd prompt
+rem just build the sln and upload the vsixes that appear in \Temp folder 
+
 rem  /S    Copy folders and subfolders
 rem  /Y    Suppress prompt to confirm overwriting a file.
 rem  /F    Display full source and destination file names while copying.
@@ -6,15 +10,13 @@ rem  /R    Overwrite read-only files.
 echo "SolutionDir" %1
 echo "ProjectDir" %2
 
-    FOR %%G IN (thelivesoccer_real) DO (
-rem FOR %%G IN (azuredevops,thelivesoccer_,thelivesoccer_real) DO (
-rem FOR %%G IN (azure,azuredevops,ch9,code,codeproject,dailydotnettips,dev_humor,dotnet,dotnetcurry,dotnetfdn,dotnetkicks,mscloud,newsycombinator,nodejs,octopusdeploy,reddittech,thelivesoccer_,thelivesoccer_real,theregister,visualstudio,vsmdev,vsts,xamarinhq) DO (
+rem FOR %%G IN (azure) DO (
+FOR %%G IN (azure,azuredevops,ch9,code,codeproject,dailydotnettips,dev_humor,dotnet,dotnetcurry,dotnetfdn,dotnetkicks,mscloud,newsycombinator,nodejs,octopusdeploy,reddittech,thelivesoccer_,thelivesoccer_real,theregister,visualstudio,vsmdev,vsts,xamarinhq) DO (
 
 	REM copy the static files to each of the projects. The manifest json file only allows paths that are children of its own directory, hence can't specify the common files themselves in the manifest json files. Also, shortcut file links won't do the trick either.
 	xcopy %2app\Scripts\* %1@%%G\app\Scripts\ /F /R /Y /S
-	xcopy %2artefacts\Screenshot.png %1@%%G\artefacts\Screenshot.png /F /R /Y
-    xcopy %2artefacts\Licence.md %1@%%G\artefacts\Licence.md /F /R /Y
-
+	xcopy %2artefacts\Screenshot.png %1@%%G\artefacts\Screenshot.png /F /R /Y /i
+    xcopy %2artefacts\Licence.md %1@%%G\artefacts\Licence.md /F /R /Y /i
 
 	REM make the new files read-only
 	attrib +R %1@%%G\app\Scripts\*
@@ -24,10 +26,12 @@ rem FOR %%G IN (azure,azuredevops,ch9,code,codeproject,dailydotnettips,dev_humor
 	start %1@%%G\app\dashboard-widget.html
 
 	REM re-gen vsix's 
+	rem vsmp_pat not used
 	cd %1@%%G\
 	tfx extension create --rev-version --manifest-globs vss-extension.json
 
 	REM move vsix's to a single folder, to make easier to upload & purge
+	rem manually upload the vsix into vsmp edit screen
 	start robocopy %2\Temp\ %2\Temp\Archive\ *.vsix /MOV	 
 	start robocopy %1@%%G\ %2\Temp\ *.vsix /MOV	 
 )
